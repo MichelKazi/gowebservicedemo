7afc87eddcf0db60fd238a569d0e8f8f8d8a90fa
Michelkazi <michel.m.kazi@gmail.com>
2020-02-06 17:08:48 -0500

    sanity checking

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/michelkazi/gowebservicedemo/controllers"
 )
 
 func main() {
+	port := 3000
 	controllers.RegisterControllers()
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	fmt.Printf("Listening on port %d", port)
 }
