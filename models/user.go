02c758891d69e19f9ce0e96ee0ef5467eba5e53c
Michelkazi <michel.m.kazi@gmail.com>
2020-02-06 16:43:01 -0500

    added methods to user model

--- models/user.go
+++ models/user.go
@@ -11,3 +11,17 @@ var (
 	users  []*User
 	nextID = 1
 )
+
+// GetUsers returns Users
+func GetUsers() []*User {
+	return users
+}
+
+// AddUser adds a user to the users slice
+// returns the user that was added
+func AddUser(u User) (User, error) {
+	u.ID = nextID
+	nextID++
+	users = append(users, &u)
+	return u, nil
+}
